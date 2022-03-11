93d4b3e39c4b314b76d9ee2ecedf9ae7fbf957fd
Andrea <andrea.gotelli@eleves.ec-nantes.fr>
2022-03-11 18:32:05 +0100

    Update in names

--- include/chebyshev_differentiation.h
+++ include/chebyshev_differentiation.h
@@ -10,14 +10,14 @@
  * \tparam t_L The length of the interval. Default 1 for the interval [0, 1]
  * \return An std::array containing the Chebyshev points
  */
-template<unsigned int t_N, unsigned int t_L=1>
-static std::array<double, t_N> ComputeChebyshevPoints()
+template<unsigned int t_number_of_chebyshev_nodes, unsigned int t_L=1>
+static std::array<double, t_number_of_chebyshev_nodes> ComputeChebyshevPoints()
 {
-    std::array<double, t_N> x;
+    std::array<double, t_number_of_chebyshev_nodes> x;
 
     unsigned int j = 0;
     std::generate(x.begin(), x.end(), [&](){
-        return (static_cast<double>(t_L)/2)*(1 +cos( M_PI * static_cast<double>(j++) / static_cast<double>(t_N-1) ));
+        return (static_cast<double>(t_L)/2)*(1 +cos( M_PI * static_cast<double>(j++) / static_cast<double>(t_number_of_chebyshev_nodes-1) ));
     });
 
     return x;
@@ -28,15 +28,15 @@ static std::array<double, t_N> ComputeChebyshevPoints()
  * \tparam t_N The number of Chebyshev points.
  * \return An std::array containing the coeffieints
  */
-template<unsigned int t_N>
-static std::array<double, t_N> GetCoefficients_c()
+template<unsigned int t_number_of_chebyshev_nodes>
+static std::array<double, t_number_of_chebyshev_nodes> GetCoefficients_c()
 {
-    std::array<double, t_N> c;
+    std::array<double, t_number_of_chebyshev_nodes> c;
 
     unsigned int i = 0;
     std::generate(c.begin(), c.end(), [&](){
         //  gain is 2 in the edges and 1 elsewhere
-        const unsigned int gain = (i==0 or i==t_N-1) ? 2 : 1;
+        const unsigned int gain = (i==0 or i==t_number_of_chebyshev_nodes-1) ? 2 : 1;
 
         //  Follows the formula
         return pow(-1, i++)*gain;
@@ -50,13 +50,13 @@ static std::array<double, t_N> GetCoefficients_c()
  * \tparam t_N The number of Chebyshev points.
  * \return The Chebyshev differentiation matrix
  */
-template<unsigned int t_N>
+template<unsigned int t_number_of_chebyshev_nodes>
 static Eigen::MatrixXd getDn()
 {
-    typedef Eigen::Matrix<double, t_N, t_N> MatrixNN;
+    typedef Eigen::Matrix<double, t_number_of_chebyshev_nodes, t_number_of_chebyshev_nodes> MatrixNN;
 
     //  Define the Chebyshev points on the unit circle
-    const auto x = ComputeChebyshevPoints<t_N>();
+    const auto x = ComputeChebyshevPoints<t_number_of_chebyshev_nodes>();
 
 
     //  Create a matrix every row filled with a point value
@@ -68,13 +68,13 @@ static Eigen::MatrixXd getDn()
 
 
     //  Now compute the array containing the coefficients used in the definition of Dn
-    const auto c = GetCoefficients_c<t_N>();
+    const auto c = GetCoefficients_c<t_number_of_chebyshev_nodes>();
 
 
     //  Create the appropriate matrix of coefficients
     MatrixNN C;
-    for(unsigned int i=0; i<t_N;i++) {
-        for(unsigned int j=0; j<t_N;j++) {
+    for(unsigned int i=0; i<t_number_of_chebyshev_nodes;i++) {
+        for(unsigned int j=0; j<t_number_of_chebyshev_nodes;j++) {
             C(i,j) = c[i]/c[j];
         }
     }
@@ -83,12 +83,12 @@ static Eigen::MatrixXd getDn()
     const MatrixNN Y = X - X.transpose() + MatrixNN::Identity();
 
     //  Declare the differentiation matrix
-    Eigen::MatrixXd  Dn(t_N, t_N);
+    Eigen::MatrixXd  Dn(t_number_of_chebyshev_nodes, t_number_of_chebyshev_nodes);
 
 
     //  Obtain off diagonal element for the differentiation matrix
-    for(unsigned int i=0; i<t_N;i++) {
-        for(unsigned int j=0; j<t_N;j++) {
+    for(unsigned int i=0; i<t_number_of_chebyshev_nodes;i++) {
+        for(unsigned int j=0; j<t_number_of_chebyshev_nodes;j++) {
             Dn(i,j) = C(i, j) / Y(i, j);
         }
     }
