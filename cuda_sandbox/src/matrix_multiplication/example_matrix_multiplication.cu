0e6872eee36a24438ab195945fec41e91a4b6292
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2023-05-09 09:58:20 -0400

    ok clean examples

--- cuda_sandbox/src/matrix_multiplication/example_matrix_multiplication.cu
+++ cuda_sandbox/src/matrix_multiplication/example_matrix_multiplication.cu
@@ -108,7 +108,10 @@ int main(int argc, char *argv[])
     The handler is an object which is used to manage the api in its threads and eventually thrown errors
 
 
-    Then there are the streams.
+    Then there are the streams. But we don't need to know what they are.
+    We do not use them for now.
+
+    If you are interested:
 
     Streams define the flow of data when copying.
     Imagine: We have 100 units of data (whatever it is) to copy from one place to another.
@@ -128,27 +131,66 @@ int main(int argc, char *argv[])
         cublasCreate(&cublasH)
     );
     
-    //  common CUDA need CUDA_CHECK
-    CUDA_CHECK(
-        cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking)
-    );
-    CUBLAS_CHECK(
-        cublasSetStream(cublasH, stream)
-    );
 
 
 /*  Now We need to initialize the matrices with some values
-    Here I am just loading stuff from saved matrices in csv format
 */
 
-    Eigen::MatrixXd A;
-    LoadEigenMatrixFromFile(A, "A.csv");
+    Eigen::MatrixXd A(6, 4);
+    A << Eigen::MatrixXd::Identity(4, 4),
+          0, 1, 0, 0,
+          0, 0, 1, 0;
+    std::cout << "A : \n" << A << "\n\n";
+   
 
-    Eigen::MatrixXd P;
-    LoadEigenMatrixFromFile(P, "P.csv");
+    Eigen::MatrixXd B(6, 3);
+    double row_coeff;
+    double col_coeff;
+    const double scale = 10.0f;
+    for(unsigned int row=0; row<B.rows(); row++){
+        row_coeff = static_cast<double>(row)/scale;
+        for(unsigned int col=0; col<B.cols(); col++){
+            col_coeff = static_cast<double>(col + 1)/scale;
 
-    Eigen::MatrixXd Ap;
-    LoadEigenMatrixFromFile(Ap, "Ap.csv");
+            B(row, col) = col_coeff + row_coeff;
+        }
+    }
+    std::cout << "B : \n" << B << "\n\n";
+
+
+    Eigen::MatrixXd C(3, 3);
+    C << 1, 2, 3,
+         1, 2, 3,
+         1, 2, 3;
+    std::cout << "C : \n" << C << "\n\n";
+
+
+    Eigen::MatrixXd D(4, 3);
+    Eigen::Vector4d base(1, 2, 3, 4);
+    for(unsigned int col=0; col<D.cols(); col++){
+        D.col(col) = base;
+        base /= 2;
+    }
+    
+    std::cout << "D : \n" << D << "\n\n";
+
+
+    //  Compute the solution 
+    const Eigen::MatrixXd BC_Eigen = B*C;
+    const Eigen::MatrixXd ATBC_Eigen = A.transpose()*BC_Eigen;
+    const Eigen::MatrixXd F_Eigen = ATBC_Eigen + D;
+
+    std::cout << "BC_Eigen : \n" << BC_Eigen << "\n\n";
+    std::cout << "ATBC_Eigen : \n" << ATBC_Eigen << "\n\n";
+    std::cout << "F_Eigen : \n" << F_Eigen << "\n\n";
+
+
+    Eigen::MatrixXd BC_CUDA = Eigen::MatrixXd::Zero(BC_Eigen.rows(), BC_Eigen.cols());
+    Eigen::MatrixXd ATBC_CUDA = Eigen::MatrixXd::Zero(ATBC_Eigen.rows(), ATBC_Eigen.cols());
+    Eigen::MatrixXd F_CUDA = Eigen::MatrixXd::Zero(F_Eigen.rows(), F_Eigen.cols());
+
+
+
 
     //  Matrices are stored in vector form by column wise
 
@@ -157,23 +199,41 @@ int main(int argc, char *argv[])
 
     Definition of matrices dimensions.
 
-    Feel free to change these names, I do not like them neither...
+    Here the leqding dimension is the number of rows.
+    This is because we are using column major order.
+    If we were using row major order, the leading dimension would be the number of columns.
+
+    https://stackoverflow.com/questions/16376804/clarification-of-the-leading-dimension-in-cublas-when-transposing
     */
     const int rows_A = A.rows();
     const int cols_A = A.cols();
-    const int rows_P = P.rows();
-    const int cols_P = P.cols();
-    const int lda = rows_A;
-    const int ldb = rows_P;
-    const int ldc = rows_P;
+    const int ld_A = rows_A;
+
+    const int rows_B = B.rows();
+    const int cols_B = B.cols();
+    const int ld_B = rows_B;
+
+    const int rows_C = C.rows();
+    const int cols_C = C.cols();
+    const int ld_C = rows_C;
+
+    const int rows_D = D.rows();
+    const int cols_D = D.cols();
+    const int ld_D = rows_D;
 
 
+    const int rows_BC = BC_Eigen.rows();
+    const int cols_BC = BC_Eigen.cols();
+    const int ld_BC = rows_BC;
 
-    Eigen::MatrixXd AP(rows_A, cols_A);
-    Eigen::MatrixXd PAP(rows_P, cols_P);
 
-    const data_type alpha = 1.0;
-    const data_type beta = 0.0;
+    const int rows_F = F_Eigen.rows();
+    const int cols_F = F_Eigen.cols();
+    const int ld_F = rows_F;
+
+    
+
+    
 
 
     /*
@@ -182,10 +242,17 @@ int main(int argc, char *argv[])
     Here we declare the pointers
 
     */
-    data_type *d_A = nullptr;
-    data_type *d_P = nullptr;
-    data_type *d_AP = nullptr;
-    data_type *d_PAP = nullptr;
+    double *d_A = nullptr;
+    double *d_B = nullptr;
+    double *d_C = nullptr;
+    double *d_D = nullptr;
+
+    //  The pointer to the solution of B * C
+    double *d_BC = nullptr;
+
+    //  The pointer for the solution of the linear equation which is done directly in cuda
+    double *d_F = nullptr;
+
 
 
 
@@ -202,31 +269,60 @@ int main(int argc, char *argv[])
     takes the parameter stream.
 
 */
+    const auto size_of_double = sizeof(double);
+    const auto size_of_A_in_bytes = size_of_double * A.size();
+    const auto size_of_B_in_bytes = size_of_double * B.size();
+    const auto size_of_C_in_bytes = size_of_double * C.size();
+    const auto size_of_D_in_bytes = size_of_double * D.size();
+
+    const auto size_of_BC_in_bytes = size_of_double * BC_Eigen.size();
+    // const auto size_of_ATBC_in_bytes = size_of_double * ATBC_Eigen.size();
+    const auto size_of_F_in_bytes = size_of_double * F_Eigen.size();
+
+
+    //  Allocate the memory
     CUDA_CHECK(
-        cudaMalloc(reinterpret_cast<void **>(&d_A), sizeof(data_type) * A.size())
+        cudaMalloc(reinterpret_cast<void **>(&d_A), size_of_A_in_bytes)
     );
     CUDA_CHECK(
-        cudaMalloc(reinterpret_cast<void **>(&d_P), sizeof(data_type) * P.size())
+        cudaMalloc(reinterpret_cast<void **>(&d_B), size_of_B_in_bytes)
     );
     CUDA_CHECK(
-        cudaMalloc(reinterpret_cast<void **>(&d_AP), sizeof(data_type) * P.size())
+        cudaMalloc(reinterpret_cast<void **>(&d_C), size_of_C_in_bytes)
     );
     CUDA_CHECK(
-        cudaMalloc(reinterpret_cast<void **>(&d_PAP), sizeof(data_type) * P.size())
+        cudaMalloc(reinterpret_cast<void **>(&d_D), size_of_D_in_bytes)
     );
 
     CUDA_CHECK(
-        cudaMemcpyAsync(d_A, A.data(), sizeof(data_type) * A.size(), cudaMemcpyHostToDevice, stream)
+        cudaMalloc(reinterpret_cast<void **>(&d_BC), size_of_BC_in_bytes)
     );
     CUDA_CHECK(
-        cudaMemcpyAsync(d_P, P.data(), sizeof(data_type) * P.size(), cudaMemcpyHostToDevice, stream)
+        cudaMalloc(reinterpret_cast<void **>(&d_F), size_of_F_in_bytes)
     );
+
+
+    //  Copy the data
+    CUDA_CHECK(
+        cudaMemcpy(d_A, A.data(), size_of_A_in_bytes, cudaMemcpyHostToDevice)
+    );
+    CUDA_CHECK(
+        cudaMemcpy(d_B, B.data(), size_of_B_in_bytes, cudaMemcpyHostToDevice)
+    );
+    CUDA_CHECK(
+        cudaMemcpy(d_C, C.data(), size_of_C_in_bytes, cudaMemcpyHostToDevice)
+    );
+    CUDA_CHECK(
+        cudaMemcpy(d_F, D.data(), size_of_D_in_bytes, cudaMemcpyHostToDevice)
+    );
+
+
 
 
 
     /* step 3: compute 
     
-    Here we just do the two operations AP= A*P and PAP = transpose(P)*AP.
+    Here we solve the system F = A.transpose() * B * C + D
 
     We use the function cublasDgemm which is composed of:
 
@@ -248,49 +344,39 @@ int main(int argc, char *argv[])
     These are respectively set as second and third parameters of the function.
     For example CUBLAS_OP_N will leave the matrix as it is wile CUBLAS_OP_T will transpose the matrix.
     alpha and beta are scaling factors. alpha can be used to scale the multiplication A*B by a constant scalar value 
-    while beta will create a recursive increment on C. In our case, beta will typically be zero.
-    rows_A rows_P and cols_A express the number of rows and columns of the involved matrices. In this case, we have squared matrices
-    thus they are all the same.
+    while beta will create a recursive increment on C. 
 
     */
-
+    double alpha = 1.0;
+    double beta = 0.0;
     CUBLAS_CHECK(
-        cublasDgemm(cublasH, CUBLAS_OP_N, CUBLAS_OP_N, rows_A, rows_P, cols_A, &alpha, d_A, lda, d_P, ldb, &beta, d_AP, ldc)
-    );
-
-    CUBLAS_CHECK(
-        cublasDgemm(cublasH, CUBLAS_OP_T, CUBLAS_OP_N, rows_A, rows_P, cols_A, &alpha, d_P, lda, d_AP, ldb, &beta, d_PAP, ldc)
+        cublasDgemm(cublasH, CUBLAS_OP_N, CUBLAS_OP_N, rows_B, rows_C, cols_B, &alpha, d_B, ld_B, d_C, ld_C, &beta, d_BC, ld_BC)
     );
 
 
-    /* step 4: copy data to host 
-    
-    see that the function is the same as before, but now I am passing cudaMemcpyDeviceToHost to copy from the device to the host (CPU)
-
-
-    This is necessary as I will need to print
-    
-    */
     CUDA_CHECK(
-        cudaMemcpyAsync(PAP.data(), d_PAP, sizeof(data_type) * P.size(), cudaMemcpyDeviceToHost,stream)
+        cudaMemcpy(BC_CUDA.data(), d_BC, size_of_BC_in_bytes, cudaMemcpyDeviceToHost)
+    );
+    std::cout << "BC_Eigen : \n" << BC_Eigen << "\n\n";
+    std::cout << "BC_CUDA : \n" << BC_CUDA << "\n\n";
+
+
+    beta = 1.0;
+    CUBLAS_CHECK( //    Pay attention here the rows of A.transpose() are the cols of A and vice versa
+        cublasDgemm(cublasH, CUBLAS_OP_T, CUBLAS_OP_N, cols_A, rows_BC, rows_A, &alpha, d_A, ld_A, d_BC, ld_BC, &beta, d_F, ld_F)
     );
 
 
-    /*
-    Before using the stuff in the host, wait for all the streams to have copied the data.
-    */
-    CUDA_CHECK(cudaStreamSynchronize(stream));
+    CUDA_CHECK(
+        cudaMemcpy(F_CUDA.data(), d_F, size_of_F_in_bytes, cudaMemcpyDeviceToHost)
+    );
+    std::cout << "F_Eigen : \n" << F_Eigen << "\n\n";
+    std::cout << "F_CUDA : \n" << F_CUDA << "\n\n";
+
+    
 
 
-
-
-    /*
-        Here If you want you can compare the results
-    */
-    Eigen::MatrixXd error = Ap - PAP;
-    std::cout<< "error = " << std::endl << error.norm() <<std::endl << std::endl;
-
-
+    std::cout << "OK completed \n\n\n\n\n";
 
     /*
 
@@ -299,20 +385,31 @@ int main(int argc, char *argv[])
     Free the memory
     */
 
-    CUDA_CHECK(cudaFree(d_A));
-    CUDA_CHECK(cudaFree(d_P));
-    CUDA_CHECK(cudaFree(d_AP));
-    CUDA_CHECK(cudaFree(d_PAP));
+    CUDA_CHECK(
+        cudaFree(d_A)
+    );
+    CUDA_CHECK(
+        cudaFree(d_B)
+    );
+    CUDA_CHECK(
+        cudaFree(d_BC)
+    );
+    CUDA_CHECK(
+        cudaFree(d_F)
+    );
 
 
     /*
     Destry cuda objects
     */
-    CUBLAS_CHECK(cublasDestroy(cublasH));
+    CUBLAS_CHECK(
+        cublasDestroy(cublasH)
+    );
 
-    CUDA_CHECK(cudaStreamDestroy(stream));
 
-    CUDA_CHECK(cudaDeviceReset());
+    CUDA_CHECK(
+        cudaDeviceReset()
+    );
 
     
 
@@ -321,5 +418,4 @@ int main(int argc, char *argv[])
 
     return EXIT_SUCCESS;
 
-return 4;
 }
